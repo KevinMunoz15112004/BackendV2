f1fd65c524ccedaca9be615ca74f2e24a36c1d1d
KevinMunoz15112004 <kevinmunoz57898@gmail.com>
2025-07-31 20:46:19 -0500

    Backend actualizado

--- src/routers/mensajesRoutes.js
+++ src/routers/mensajesRoutes.js
@@ -1,12 +1,33 @@
 import express from 'express';
 import Mensaje from '../models/Mensajes.js';
+import { verificarTokenEstudiante } from '../middlewares/JWTEstudiante.js';
+import mongoose from 'mongoose';
+import Estudiantes from '../models/Estudiantes.js';
 
 const router = express.Router();
 
-router.get('/mensajes/historial/:estudianteA/:estudianteB', async (req, res) => {
+router.get('/mensajes/historial/:estudianteA/:estudianteB', verificarTokenEstudiante, async (req, res) => {
   const { estudianteA, estudianteB } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(estudianteA) || !mongoose.Types.ObjectId.isValid(estudianteB)) {
+    return res.status(400).json({ msg: 'Uno o ambos IDs no son válidos' })
+  }
+
+  if (!estudianteA || !estudianteB) {
+    return res.status(400).json({ msg: 'Faltan parámetros requeridos en la URL' })
+  }
+
   try {
+
+     const [existeA, existeB] = await Promise.all([
+      Estudiantes.exists({ _id: estudianteA }),
+      Estudiantes.exists({ _id: estudianteB })
+    ])
+
+    if (!existeA || !existeB) {
+      return res.status(404).json({ msg: 'Uno o ambos estudiantes no existen' })
+    }
+
     const mensajes = await Mensaje.find({
       $or: [
         { autor: estudianteA, destinatario: estudianteB },
